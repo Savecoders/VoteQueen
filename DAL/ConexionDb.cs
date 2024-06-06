8f9a4e30bdc6281ac6d8c72c99bce4de2cb4f231
Savecoders <pablopincay2003@gmail.com>
2024-06-05 20:07:55 -0500

    Add: Data layer connect sql server

---
+++ DAL/ConexionDb.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+
+namespace UgVoteQueen.DAL
+{
+    internal class ConexionDb
+    {
+        // Reemplazar por la cadena de conexión a la base de datos
+        private readonly string _connectionString = "Server=.\\DESARROLLO;Database=UgQueenDataBase;Trusted_Connection=True;Integrated Security=True;TrustServerCertificate=True;";
+
+        private readonly SqlConnection connection;
+
+        public ConexionDb()
+        {
+            connection = new SqlConnection(_connectionString);
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+        // Ejecutar consultas
+        public bool EjecutarConsulta(string consulta, SqlParameter[] parametros)
+        {
+            try
+            {
+
+                SqlCommand command = new SqlCommand(consulta, connection);
+                command.Parameters.AddRange(parametros);
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public DataSet EjecutarConsultaSelect(string consulta)
+        {
+            DataSet dataSet = new DataSet();
+            try
+            {
+                SqlCommand command = new(consulta, connection);
+                SqlDataAdapter adapter = new(command);
+                connection.Open();
+                adapter.Fill(dataSet);
+                connection.Close();
+                return dataSet;
+
+            }
+            catch
+            {
+                return dataSet;
+            }
+
+        }
+    }
+}
---
+++ DAL/UsuarioDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+
+
+namespace UgVoteQueen.DAL
+{
+    class UsuarioDAL
+    {
+        private readonly ConexionDb conexion;
+        public UsuarioDAL()
+        {
+            conexion = new ConexionDb();
+        }
+
+        public bool RegistrarEstudiante(
+            string nombre,
+            string correo,
+            string contrasena,
+            byte[] fotoPerfil
+        )
+        {
+            // consulta procedure
+            string consulta = "RegistrarEstudiante";
+
+            SqlParameter[] parametros = [
+                new SqlParameter("@Nombre", nombre),
+                new SqlParameter("@Correo", correo),
+                new SqlParameter("@Contrasena", contrasena),
+                new SqlParameter("@FotoPerfil", fotoPerfil)
+            ];
+
+            // ejecutar consulta
+            return conexion.EjecutarConsulta(consulta, parametros);
+        }
+
+    }
+}
