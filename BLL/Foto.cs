9c0558f135f78bf5e6ec559594dec27101159b66
Savecoders <pablopincay2003@gmail.com>
2024-06-08 10:13:27 -0500

    chore: Remove unused GaleriaFoto class and update Foto class

--- BLL/Foto.cs
+++ BLL/Foto.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace UgVoteQueen.BLL
 {
-    internal class Foto
+    public class Foto
     {
         public int ID { get; set; }
         public required byte[] Imagen { get; set; }
+
+        public void SetImagen(byte[] imagen)
+        {
+            if (imagen == null)
+            {
+                throw new ArgumentException("La imagen no puede estar vacia");
+            }
+            Imagen = imagen;
+        }
     }
 }
