f03961ab2cb8b05716127b2635b3fcb9c65ad01b
Savecoders <pablopincay2003@gmail.com>
2024-06-10 17:30:53 -0500

    feat: Add Register and Login PL

---
+++ PL/Session/Login.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UgVoteQueen.Common;
+
+namespace UgVoteQueen.PL.Session
+{
+    public partial class Login : UserControl
+    {
+        private Register registerPanel;
+        private Panel mainPanel;
+        
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPanels(Panel mainPanel, Register registerPanel)
+        {
+            this.mainPanel = mainPanel;
+            this.registerPanel = registerPanel;
+        }
+
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FlexLayout_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            Utils utils = new Utils();
+            utils.ReplacePanelContent(mainPanel, registerPanel);
+        }
+    }
+}
---
+++ PL/Session/Register.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UgVoteQueen.Common;
+
+namespace UgVoteQueen.PL.Session
+{
+    public partial class Register : UserControl
+    {
+
+        private Panel mainPanel;
+        private Login loginPanel;
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPanels(Panel mainPanel, Login loginPanel)
+        {
+            this.mainPanel = mainPanel;
+            this.loginPanel = loginPanel;
+        }
+
+        private void lLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LLSesion_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Utils utils = new Utils();
+            utils.ReplacePanelContent(mainPanel, loginPanel);
+        }
+    }
+}
