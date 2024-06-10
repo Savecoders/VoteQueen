54fcccf728e20e266ff7429632bede79811cf448
Savecoders <pablopincay2003@gmail.com>
2024-06-09 20:55:47 -0500

    Refactor database connection and query execution methods

--- BLL/Candidata.cs
+++ BLL/Candidata.cs
@@ -9,110 +9,135 @@ namespace UgVoteQueen.BLL
     public class Candidata
     {
         public int ID { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-
+        private string nombre;
+        private int edad;
+        private string datosAcademicos;
+        private string pasatiempos;
+        private string habilidades;
+        private string intereses;
+        private string aspiraciones;
         public byte[] FotoPrincipal { get; set; } = [];
-
-        public int Edad { get; private set; } = 0;
-
-        public string DatosAcademicos { get; set; } = string.Empty;
-
-        public string Pasatiempos { get; set; } = string.Empty;
-
-        public string Habilidades { get; set; } = string.Empty;
-
-        public string Intereses { get; set; } = string.Empty;
-
-        public string Aspiraciones { get; private set; } = string.Empty;
-
         public List<Foto> GaleriaFotos { get; set; }
-
         public List<Comentario> Comentarios { get; set; }
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("El nombre no puede estar vacio");
+                }
+
+                nombre = value;
+            }
+        }
+
+
+        public int Edad
+        {
+            get { return edad; }
+            set
+            {
+                if (value < 18)
+                {
+                    throw new ArgumentException("La edad no puede ser menor a 18 años");
+                }
+
+                edad = value;
+            }
+        }
+
+        public string DatosAcademicos
+        {
+            get { return datosAcademicos; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Los datos academicos no pueden estar vacios");
+                }
+
+                datosAcademicos = value;
+            }
+        }
+
+        public string Pasatiempos
+        {
+            get { return pasatiempos; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Los pasatiempos no pueden estar vacios");
+                }
+
+                pasatiempos = value;
+            }
+        }
+
+        public string Habilidades
+        {
+            get { return habilidades; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Las habilidades no pueden estar vacias");
+                }
+
+                habilidades = value;
+            }
+        }
+
+        public string Intereses
+        {
+            get { return intereses; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Los intereses no pueden estar vacios");
+                }
+
+                intereses = value;
+            }
+        }
+
+        public string Aspiraciones
+        {
+            get
+            {
+                return aspiraciones;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Las aspiraciones no pueden estar vacias");
+                }
+
+                aspiraciones = value;
+            }
+        }
+
 
 
         // Constructor
 
         public Candidata(string nombre, int edad, string datosAcademicos, string pasatiempos, string habilidades, string intereses, string aspiraciones)
         {
-            SetNombre(nombre);
-            SetEdad(edad);
-            SetDatosAcademicos(datosAcademicos);
-            SetPasatiempos(pasatiempos);
-            SetHabilidades(habilidades);
-            SetIntereses(intereses);
-            SetAspiraciones(aspiraciones);
-        }
-
-        public void SetNombre(string nombre)
-        {
-            if (string.IsNullOrWhiteSpace(nombre))
-            {
-                throw new ArgumentException("El nombre no puede estar vacio");
-            }
-
             Nombre = nombre;
-        }
-
-
-        public void SetEdad(int edad)
-        {
-            if (edad < 18)
-            {
-                throw new ArgumentException("La edad no puede ser menor a 18 años");
-            }
-
             Edad = edad;
-        }
-
-        public void SetDatosAcademicos(string datosAcademicos)
-        {
-            if (string.IsNullOrWhiteSpace(datosAcademicos))
-            {
-                throw new ArgumentException("Los datos academicos no pueden estar vacios");
-            }
-
             DatosAcademicos = datosAcademicos;
-        }
-
-        public void SetPasatiempos(string pasatiempos)
-        {
-            if (string.IsNullOrWhiteSpace(pasatiempos))
-            {
-                throw new ArgumentException("Los pasatiempos no pueden estar vacios");
-            }
-
             Pasatiempos = pasatiempos;
-        }
-
-        public void SetHabilidades(string habilidades)
-        {
-            if (string.IsNullOrWhiteSpace(habilidades))
-            {
-                throw new ArgumentException("Las habilidades no pueden estar vacias");
-            }
-
             Habilidades = habilidades;
-        }
-
-        public void SetIntereses(string intereses)
-        {
-            if (string.IsNullOrWhiteSpace(intereses))
-            {
-                throw new ArgumentException("Los intereses no pueden estar vacios");
-            }
-
             Intereses = intereses;
-        }
-
-        public void SetAspiraciones(string aspiraciones)
-        {
-            if (string.IsNullOrWhiteSpace(aspiraciones))
-            {
-                throw new ArgumentException("Las aspiraciones no pueden estar vacias");
-            }
-
             Aspiraciones = aspiraciones;
         }
+
 
         public void SetFotoPrincipal(byte[] foto)
         {
