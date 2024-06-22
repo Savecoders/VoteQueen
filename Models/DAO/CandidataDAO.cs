3191cc07ef0e269fada4cda3f75f05a95de2d0bf
Savecoders <pablopincay2003@gmail.com>
2024-06-22 13:12:01 -0500

    Refactor: Update namespace and file structure for DAO classes

---
+++ Models/DAO/CandidataDAO.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+using UgVoteQueen.Models.DTO;
+
+namespace UgVoteQueen.Models.DAO
+{
+    class CandidataController
+    {
+
+        private readonly ConexionDb conexion;
+
+        public CandidataController()
+        {
+            conexion = new ConexionDb();
+        }
+
+        public bool Registrar(
+            Candidata candidata
+        )
+        {
+            // consulta procedure
+            string consulta = "sp_registrar_candidata";
+
+            SqlParameter[] parametros = [
+                new SqlParameter("@Nombre", candidata.Nombre),
+                new SqlParameter("@FotoPrincipal", candidata.FotoPrincipal),
+                new SqlParameter("@Edad", candidata.Edad),
+                new SqlParameter("@DatosAcademicos", candidata.DatosAcademicos),
+                new SqlParameter("@Pasatiempos", candidata.Pasatiempos),
+                new SqlParameter("@Habilidades", candidata.Habilidades),
+                new SqlParameter("@Intereses", candidata.Intereses),
+                new SqlParameter("@Aspiraciones", candidata.Aspiraciones)
+            ];
+
+            // ejecutar consulta
+            return conexion.EjecutarProcedure(consulta, parametros);
+        }
+
+        public bool Actualizar(
+            Candidata candidata
+        )
+        {
+            // consulta procedure
+            string consulta = "sp_actualizar_candidata";
+
+            SqlParameter[] parametros = [
+                new SqlParameter("@Id", candidata.ID),
+                new SqlParameter("@Nombre", candidata.Nombre),
+                new SqlParameter("@FotoPrincipal", candidata.FotoPrincipal),
+                new SqlParameter("@Edad", candidata.Edad),
+                new SqlParameter("@DatosAcademicos", candidata.DatosAcademicos),
+                new SqlParameter("@Pasatiempos", candidata.Pasatiempos),
+                new SqlParameter("@Habilidades", candidata.Habilidades),
+                new SqlParameter("@Intereses", candidata.Intereses),
+                new SqlParameter("@Aspiraciones", candidata.Aspiraciones)
+            ];
+
+            // ejecutar consulta
+            return conexion.EjecutarProcedure(consulta, parametros);
+        }
+
+        public bool Eliminar(int Id)
+        {
+            // consulta procedure
+            string consulta = "sp_eliminar_candidata";
+
+            SqlParameter[] parametros = [
+                new SqlParameter("@CandidataID", Id)
+            ];
+
+            // ejecutar consulta
+            return conexion.EjecutarProcedure(consulta, parametros);
+        }
+
+        public DataSet Buscar(int Id)
+        {
+            string query = "SELECT * FROM Candidata WHERE CandidataID = " + Id;
+            return conexion.EjecutarConsultaSelect(query);
+        }
+
+        public DataSet Buscar(string Nombre)
+        {
+            string query = "SELECT * FROM Candidata WHERE Nombre = " + Nombre;
+            return conexion.EjecutarConsultaSelect(query);
+        }
+
+        public DataSet ListarCandidatas()
+        {
+            string query = "SELECT * FROM Candidata";
+            return conexion.EjecutarConsultaSelect(query);
+        }
+
+    }
+
+}
---
+++ Models/DAO/EstudianteDAO.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using Microsoft.Data.SqlClient;
+using UgVoteQueen.Models.DTO;
+
+
+namespace UgVoteQueen.Models.DAO
+{
+    class EstudianteDAO
+    {
+        private readonly ConexionDb conexion;
+        public EstudianteDAO()
+        {
+            conexion = new ConexionDb();
+        }
+
+        public bool RegistrarEstudiante(
+            Estudiante estudiante
+        )
+        {
+
+            if (this.UsuarioExiste(estudiante))
+            {
+                throw new Exception("El usuario ya existe");
+            }
+
+            // consulta procedure
+            string consulta = "sp_registrar_estudiante";
+
+            SqlParameter[] parametros = [
+                new SqlParameter("@Nombre", estudiante.Nombre),
+                new SqlParameter("@Correo", estudiante.Correo),
+                new SqlParameter("@Contrasena", estudiante.Contrasena),
+                new SqlParameter("@FotoPerfil", estudiante.FotoPerfil),
+                new SqlParameter("@Matricula", estudiante.Matricula)
+            ];
+
+            // ejecutar consulta
+            return conexion.EjecutarProcedure(consulta, parametros);
+        }
+
+        public DataSet LoginUsuario(Estudiante estudiante)
+        {
+            string query = "SELECT * FROM Estudiante WHERE Correo = '" + estudiante.Correo + "' AND Contrasena = '" + estudiante.Contrasena + "'";
+            return conexion.EjecutarConsultaSelect(query);
+        }
+
+        public bool ActualizarEstudiante(
+            Estudiante estudiante
+        )
+        {
+            // consulta procedure
+            string consulta = "sp_actualizar_estudiante";
+
+            SqlParameter[] parametros = [
+                new SqlParameter("@Id", estudiante.ID),
+                new SqlParameter("@Nombre", estudiante.Nombre),
+                new SqlParameter("@Correo", estudiante.Correo),
+                new SqlParameter("@Contrasena", estudiante.Contrasena),
+                new SqlParameter("@FotoPerfil", estudiante.FotoPerfil),
+                new SqlParameter("@Matricula", estudiante.Matricula),
+            ];
+
+            // ejecutar consulta
+            return conexion.EjecutarProcedure(consulta, parametros);
+        }
+
+        public bool UsuarioExiste(Estudiante estudiante)
+        {
+            string query = "SELECT * FROM Estudiante WHERE Correo = '" + estudiante.Correo + "'";
+            return conexion.EjecutarConsulta(query);
+        }
+
+    }
+}
