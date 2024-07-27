5e51b3644ba60417e99f5f0e3109e1461ffdacbd
Savecoders <pablopincay2003@gmail.com>
2024-07-26 23:33:10 -0500

    Change Card Component

--- UgVoteQueen/Common/Utils.cs
+++ UgVoteQueen/Common/Utils.cs
@@ -9,12 +9,21 @@ namespace Views.Common
     public class Utils
     {
 
-        public void ReplacePanelContent(Panel panel, UserControl userControl)
+        public static void ReplacePanelContent(Panel panel, UserControl userControl)
         {
             panel.Controls.Clear();
             userControl.Dock = DockStyle.Fill;
             panel.Controls.Add(userControl);
             userControl.BringToFront();
+        }
+
+        public static Image ConvertImage(byte[] image)
+        {
+            using (MemoryStream ms = new MemoryStream(image))
+            {
+                return Image.FromStream(ms);
+            }
+            
         }
 
     }
---
+++ UgVoteQueen/Components/CardCandidata.cs
@@ -0,0 +1,36 @@
+﻿using Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Views.Common;
+
+namespace Views.Components
+{
+    public partial class CardCandidata : UserControl
+    {
+        public CardCandidata()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CardCandidata_Load(object sender, EventArgs e, Candidata candidata)
+        {
+
+            LCandidataNombre.Text = candidata.Nombre;
+            LCarrera.Text = candidata.Carrera;
+            ImageCandidata.Image = Utils.ConvertImage(candidata.FotoPrincipal);
+            LComentarios.Text = candidata.Comentarios.Count.ToString();
+            LayoutState.Controls.Add(pictureBox2);
+            LayoutState.Controls.Add(LComentarios);
+
+        }
+
+    }
+}
